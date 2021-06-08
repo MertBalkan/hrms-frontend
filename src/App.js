415542bff115de9bc83b4d616dd4de3ca01d2b2e
Mert Balkan <43827959+MertBalkan@users.noreply.github.com>
2021-06-08 14:17:36 +0300

    init

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import 'semantic-ui-css/semantic.min.css'
+import { Menu } from 'semantic-ui-react'
+function App() {
+  return (
+    <div className="App">
+   
+    </div>
+  );
+}
+
+export default App;
